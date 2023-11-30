2122500c678acd30074a19c07936639f2a0ab13f
Kamil Jakubisiak <u87_kamjak_waw@technischools.com>
2023-11-30 21:47:03 +0100

    TESTY

---
+++ app_test.py
@@ -0,0 +1,48 @@
+import pytest
+from flask.testing import FlaskClient
+from app import app, users
+
+STATUS_OK = 200
+CREATED = 201
+NOT_FOUND = 404
+
+@pytest.fixture
+def client() -> FlaskClient:
+    return app.test_client()
+
+def test_ping(client: FlaskClient) -> None:
+    expected_response = 'Pong!'
+    actual = client.get('/ping')
+    assert actual.data.decode('utf-8') == expected_response
+
+def test_get_user_list(client: FlaskClient) -> None:
+    actual = client.get('/users')
+    assert actual.json == users
+    assert actual.status_code == STATUS_OK
+
+def test_get_user(client: FlaskClient) -> None:
+    user_id = 1
+    actual = client.get(f'/users/{user_id}')
+    assert actual.json == users[user_id - 1]
+
+def test_get_user_with_no_user(client: FlaskClient) -> None:
+    user_id = len(users) + 2
+    actual = client.get(f"/users/{user_id}")
+    assert actual.status_code == NOT_FOUND
+
+def test_create_user(client: FlaskClient) -> None:
+    new_data = {"name": "abc", "lastname": "def"}
+    actual = client.post('/users', json=new_data)
+    assert actual.status_code == CREATED
+
+def test_update_user(client: FlaskClient) -> None:
+    user_id = 1
+    new_data = {"name": "Jane", "lastname": "Doe"}
+    actual = client.patch(f"/users/{user_id}", json=new_data)
+    assert actual.status_code == STATUS_OK
+
+
+def test_delete_user(client: FlaskClient) -> None:
+    user_id = 7
+    actuall = client.delete(f"/users/{user_id}")
+    assert actuall.status_code == STATUS_OK
