d5de09e7f7c28b5137c33890c000a6972b8ac609
Kamil Jakubisiak <u87_kamjak_waw@technischools.com>
2023-11-30 21:43:53 +0100

    finall_comit_app

--- app.py
+++ app.py
@@ -49,13 +49,29 @@ def update_user_partial(user_id):
     return jsonify(), 404
 
 @app.route("/users/<int:user_id>", methods=["PUT"])
-def update_user(user_id):
-    data = request.get_json()
+def create_or_update_user(user_id):
     user = find_user_by_id(user_id)
+    data = request.get_json()
     if user:
         user["name"] = data.get("name", user["name"])
         user["lastname"] = data.get("lastname", user["lastname"])
-        return "", 204
+        return " have no idea what's wrong with me :(", 204
+    elif "name" in data and "lastname" in data:
+        new_user = {
+            "id": user_id,
+            "name": data["name"],
+            "lastname": data["lastname"]
+        }
+        users.append(new_user)
+        return jsonify({"id": new_user["id"]}), 201
+    return jsonify(), 400
+
+@app.route("/users/<int:user_id>", methods=["DELETE"])
+def delete_user(user_id):
+    user = find_user_by_id(user_id)
+    if user:
+        users.remove(user)
+        return jsonify(), 204
     return jsonify(), 404
 
 def find_user_by_id(user_id):
